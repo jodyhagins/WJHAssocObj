676b1a4c7647b1418b2977433c05b23cddbac004
Jody Hagins <jody.hagins@cplusplusonline.com>
2015-08-17 11:16:53 -0400

    Initial Commit

---
+++ WJHAssocObj/WJHAssocObj.h
@@ -0,0 +1,19 @@
+//
+//  WJHAssocObj.h
+//  WJHAssocObj
+//
+//  Created by Jody Hagins on 8/17/15.
+//  Copyright (c) 2015 Jody Hagins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WJHAssocObj.
+FOUNDATION_EXPORT double WJHAssocObjVersionNumber;
+
+//! Project version string for WJHAssocObj.
+FOUNDATION_EXPORT const unsigned char WJHAssocObjVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WJHAssocObj/PublicHeader.h>
+
+
